0870a1633607460c750db51728a201b35e1e3c49
Asa <94348484+AzzamSyakir@users.noreply.github.com>
2024-01-23 01:52:02 +0000

    creating test GetUser first try

--- test/user_test.go
+++ test/user_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -209,69 +210,73 @@ func TestFetchUser(t *testing.T) {
 	t.Log("Tes berhasil")
 }
 
-// func TestGetUser(t *testing.T) {
-// 	// call SetupTest function
-// 	TestSetup(t)
-// 	// Buat mock untuk http.Request
-// 	request := httptest.NewRequest(http.MethodGet, "/api/users/{id}", bytes.NewBufferString(`{"id": "0658b09c-6fbf-4eff-8aea-3243f837b09a"}`)
-// 	tambah parameter id dengan value = `{"id": "0658b09c-6fbf-4eff-8aea-3243f837b09a"}`
+func TestGetUser(t *testing.T) {
+	// call SetupTest function
+	TestSetup(t)
 
-// 	// Set authorization header with the token
-// 	request.Header.Set("Authorization", "Bearer "+loginToken)
-// 	request.Header.Set("Content-Type", "application/json")
-// 	request.Header.Set("Accept", "application/json")
+	// Buat mock untuk http.Request
+	userID := "0658b09c-6fbf-4eff-8aea-3243f837b09a"
+	requestURL := fmt.Sprintf("/api/users/%s", userID)
+	request := httptest.NewRequest(http.MethodGet, requestURL, nil)
 
-// 	// Buat mock untuk http.ResponseWriter
-// 	recorder := httptest.NewRecorder()
+	// Set authorization header with the token
+	request.Header.Set("Authorization", "Bearer "+loginToken)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Accept", "application/json")
 
-// 	// Buat mock db
-// 	userRepository := repositories.NewUserRepository(globalDB) //using globalDB that declare in setupTest
-// 	userService := service.NewUserService(*userRepository)
-// 	userController := controller.NewUserController(*userService)
+	// Buat mock untuk http.ResponseWriter
+	recorder := httptest.NewRecorder()
 
-// 	// Panggil fungsi controller
-// 	userController.FetchUserController(recorder, request)
+	// Buat mock db
+	userRepository := repositories.NewUserRepository(globalDB) // menggunakan globalDB yang dideklarasikan di setupTest
+	userService := service.NewUserService(*userRepository)
+	userController := controller.NewUserController(*userService)
 
-// 	response := recorder.Result() // Dapatkan respons
+	// Panggil fungsi controller
+	userController.FetchUserController(recorder, request)
 
-// 	// Periksa status code
-// 	if response.StatusCode != http.StatusOK {
-// 		var result map[string]interface{}
-// 		err := json.NewDecoder(response.Body).Decode(&result)
-// 		if err != nil {
-// 			t.Fatalf("Error decoding response body: %v", err)
-// 		}
+	response := recorder.Result() // Dapatkan respons
 
-// 		errorMessage, ok := result["message"].(string)
-// 		if !ok {
-// 			t.Fatalf("Expected status code 200, got %d", response.StatusCode)
-// 		} else {
-// 			t.Fatalf("Expected status code 200, got %d. Error message: %s", response.StatusCode, errorMessage)
-// 		}
-// 		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
-// 		return
-// 	}
+	// Periksa status code
+	if response.StatusCode != http.StatusOK {
+		var result map[string]interface{}
+		err := json.NewDecoder(response.Body).Decode(&result)
+		if err != nil {
+			t.Fatalf("Error decoding response body: %v", err)
+		}
 
-// 	// Continue with other checks as needed
+		errorMessage, ok := result["message"].(string)
+		if !ok {
+			t.Fatalf("Expected status code 200, got %d", response.StatusCode)
+		} else {
+			t.Fatalf("Expected status code 200, got %d. Error message: %s", response.StatusCode, errorMessage)
+		}
 
-// 	// Periksa body respons
-// 	var result map[string]interface{}
-// 	err := json.NewDecoder(response.Body).Decode(&result)
-// 	if err != nil {
-// 		t.Fatalf("Error decoding response body: %v", err)
-// 		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
-// 		return
-// 	}
+		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
+		return
+	}
 
-// 	// Periksa message
-// 	expectedMessage := "Success" // Sesuaikan dengan pesan yang diinginkan
-// 	actualMessage, ok := result["message"].(string)
-// 	if !ok {
-// 		t.Fatalf("Pesan tidak ditemukan dalam respons")
-// 		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
-// 		return
-// 	}
-// 	assert.Equal(t, expectedMessage, actualMessage)
+	// Continue with other checks as needed
 
-// 	t.Log("Tes berhasil")
-// }
+	// Periksa body respons
+	var result map[string]interface{}
+	err := json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		t.Fatalf("Error decoding response body: %v", err)
+		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
+		return
+	}
+
+	// Periksa message
+	expectedMessage := "Success" // Sesuaikan dengan pesan yang diinginkan
+	actualMessage, ok := result["message"].(string)
+	if !ok {
+		t.Fatalf("Pesan tidak ditemukan dalam respons")
+		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
+		return
+	}
+
+	assert.Equal(t, expectedMessage, actualMessage)
+
+	t.Log("Tes berhasil")
+}
