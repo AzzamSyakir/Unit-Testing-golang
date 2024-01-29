dbd06cac028665071585a52a41bc1ca0b0ab5164
Asa <94348484+AzzamSyakir@users.noreply.github.com>
2024-01-29 01:21:59 +0000

    add Set authorization header with the token

--- test/user_test.go
+++ test/user_test.go
@@ -87,6 +87,7 @@ func TestLoginApi(t *testing.T) {
 		t.Fatal(err)
 	}
 	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer "+loginToken) // Set authorization header with the token
 
 	// Kirim request
 	response, err := client.Do(request)
@@ -184,58 +185,6 @@ func TestFetchUserApi(t *testing.T) {
 	err = json.NewDecoder(response.Body).Decode(&result)
 	if err != nil {
 		t.Fatalf("Error decoding response body: %v", err)
-	}
-
-}
-func TestFetchUser(t *testing.T) {
-	// call SetupTest function
-	TestSetup(t)
-	// Buat mock untuk http.Request
-	request := httptest.NewRequest("GET", "/users", nil)
-
-	// Set authorization header with the token
-	request.Header.Set("Authorization", "Bearer "+loginToken)
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Accept", "application/json")
-
-	// Buat mock untuk http.ResponseWriter
-	recorder := httptest.NewRecorder()
-
-	// Buat mock db
-	userRepository := repositories.NewUserRepository(globalDB) // menggunakan globalDB yang dideklarasikan di setupTest
-	userService := service.NewUserService(*userRepository)
-	userController := controller.NewUserController(*userService)
-
-	// Panggil fungsi controller
-	userController.FetchUserController(recorder, request)
-
-	response := recorder.Result() // Dapatkan respons
-	// Periksa status code
-	if response.StatusCode != http.StatusOK {
-		var result map[string]interface{}
-		err := json.NewDecoder(response.Body).Decode(&result)
-		if err != nil {
-			t.Fatalf("Error decoding response body: %v", err)
-		}
-
-		errorMessage, ok := result["message"].(string)
-		if !ok {
-			t.Fatalf("Expected status code 200, got %d", response.StatusCode)
-		} else {
-			t.Fatalf("Expected status code 200, got %d. Error message: %s", response.StatusCode, errorMessage)
-		}
-
-		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
-		return
-	}
-
-	// Periksa body respons
-	var result map[string]interface{}
-	err := json.NewDecoder(response.Body).Decode(&result)
-	if err != nil {
-		t.Fatalf("Error decoding response body: %v", err)
-		t.FailNow() // Menghentikan eksekusi tes saat ada kesalahan
-		return
 	}
 }
 func TestGetUser(t *testing.T) {
